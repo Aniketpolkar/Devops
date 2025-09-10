5e12c18a61495738f95c08a671cac482a09f3add
Aniket <aniketpolkar@gmail.com>
2025-09-11 00:30:41 +0530

    code added in run file

--- run.py
+++ run.py
@@ -1,2 +1,13 @@
 from app import app
 app.run(debug=True, host='0.0.0.0', port=80)
+
+
+import math
+def circle(radius):
+    circum = 2 * math.pi * radius
+    area =  math.pi * radius ** 2
+    return area,circum
+
+a, c = circle(3)
+print("Area is " ,round(a))
+print("Circumference is ",c)
